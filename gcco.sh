a8d50f4150f1c56718df77c80931f0d0ecd4fbf3
ssdrum <dipaololuigi95@gmail.com>
2022-11-11 18:41:57 +0000

    Added execute functionality to gcco

--- gcco.sh
+++ gcco.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 
-# Usage: gcco
-# Description: Compiles the last modified file in the current directory if it
-# is a C program. The executable's name is the same as the file's name without
-# the .c extension.
+# NAME
+#      gcco - compile last modified c program 
 #
+# SYNOPSIS
+#      gcco [-e]
 #
+# DESCRIPTION
+#      gcco compiles the last modified file in the current directory if it
+#      is a C program. The executable's name is the same as the file's name without
+#      the .c extension.
+#
+# The options are as follows:
+#
+# -e      Executes compiled program
+
+
 # ls separates output by newlines when piped by default
 input=$( ls -t | head -n 1 ) 
 
-if [[ $input == *.c ]]
+if [ $input == *.c ]
 then
     echo $input
     output=$( echo $input | cut -d . -f 1 )
     gcc -o $output $input
+
+    if [ $1 == "-e" ]
+    then
+        ./$output
+    fi
 else
     echo $input is not a C program
 fi
